ac3b0e34adbf736313c2fbbc1a77a1073fdcd651
Takanobu Suzuki <nobublack555@gmail.com>
2024-09-08 03:36:44 -0700

    fix prisma

--- src/utils/api/db.ts
+++ src/utils/api/db.ts
@@ -1,17 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
     log: ['query'],
   })
-}
-
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>
-} & typeof global
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
 export default prisma
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
