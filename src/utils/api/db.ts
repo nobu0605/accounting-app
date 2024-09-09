2782e014e4c276da3e21ae3fb890455c8664fc58
Takanobu Suzuki <nobublack555@gmail.com>
2024-09-08 21:28:09 -0700

    fix prisma

--- src/utils/api/db.ts
+++ src/utils/api/db.ts
@@ -1,13 +1,53 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+// const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'],
+// export const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     log: ['query'],
+//     datasources: {
+//       db: {
+//         url: databaseURL,
+//       },
+//     },
+//   })
+
+const { DATABASE_URL } = process.env
+
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+  interface BigInt {
+    toJSON(): string
+  }
+}
+
+let prisma: PrismaClient
+
+const databaseURL = DATABASE_URL
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: databaseURL,
+      },
+    },
   })
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient({
+      datasources: {
+        db: {
+          url: databaseURL,
+        },
+      },
+    })
+  }
+  prisma = global.prisma
+}
 
 export default prisma
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
