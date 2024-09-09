7cfce9eb11d60ef80bad556484603f79dde50888
Takanobu Suzuki <nobublack555@gmail.com>
2024-09-08 22:29:59 -0700

    fix prisma

--- src/utils/api/db.ts
+++ src/utils/api/db.ts
@@ -1,52 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 
-// const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
-// export const prisma =
-//   globalForPrisma.prisma ||
-//   new PrismaClient({
-//     log: ['query'],
-//     datasources: {
-//       db: {
-//         url: databaseURL,
-//       },
-//     },
-//   })
-
 const { DATABASE_URL } = process.env
 
-declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined
-  interface BigInt {
-    toJSON(): string
-  }
-}
-
-
-const databaseURL = DATABASE_URL
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient({
-//     datasources: {
-//       db: {
-//         url: databaseURL,
-//       },
-//     },
-//   })
-// } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient({
-      datasources: {
-        db: {
-          url: databaseURL,
-        },
-      },
-    })
-  }
-  const prisma = global.prisma
-// }
+const prisma = new PrismaClient({
+  log: ['query'],
+  datasources: {
+    db: {
+      url: DATABASE_URL,
+    },
+  },
+})
 
 export default prisma
-
-// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
