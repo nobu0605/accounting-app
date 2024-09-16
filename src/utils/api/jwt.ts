7094d187f048e1fc2d75025001f1bd9624f2f11e
Takanobu Suzuki <nobublack555@gmail.com>
2024-09-15 17:54:26 -0700

    fix jwt

--- src/utils/api/jwt.ts
+++ src/utils/api/jwt.ts
@@ -14,7 +14,7 @@ export const signJwt = async (payload: JWTPayload): Promise<string> => {
 
   return await new SignJWT(payload)
     .setProtectedHeader({ alg: 'HS256' })
-    .setExpirationTime('3d')
+    .setExpirationTime('1h')
     .sign(secret)
 }
 
