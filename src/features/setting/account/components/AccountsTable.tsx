a464852d0a144c7d553e8e6e1e9f1bad9409d58f
Takanobu Suzuki <nobublack555@gmail.com>
2024-09-29 13:50:45 -0700

    feature/account-delete

--- src/features/setting/account/components/AccountsTable.tsx
+++ src/features/setting/account/components/AccountsTable.tsx
@@ -1,45 +1,85 @@
 'use client'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
-import React from 'react'
+import { TableRow, TableHead, TableCell, TableBody, Table } from '@mui/material'
+import React, { useState } from 'react'
 import { styled } from 'styled-components'
+import { mutate } from 'swr'
+import { Button } from '@/components/ui/Button'
 import { Flex } from '@/components/ui/Flex'
+import { Snackbar } from '@/components/ui/Snackbar'
 import { Account } from '@/features/journalEntry/types/account'
+import axios from '@/utils/client/axios'
 
 type Props = {
   accounts: Account[]
 }
 
 export function AccountsTable({ accounts }: Props) {
+  const [snackbarType, setSnackbarType] = useState<'error' | 'success' | null>(null)
+
+  async function handleDeleteAccount(accountId: bigint) {
+    if (!confirm('Are you sure you want to delete this account?')) {
+      return
+    }
+
+    try {
+      await axios.delete(`/accounts/${accountId}`)
+      await mutate('/accounts')
+      setSnackbarType('success')
+    } catch (error) {
+      console.error('error: ', error)
+      setSnackbarType('error')
+    }
+  }
+
   return (
-    <StyledWrapperFlex $direction='column' $gap='15px'>
-      <div>
-        <StyledTitleSpan>Accounts setting</StyledTitleSpan>
-      </div>
-      <Table>
-        <TableHead>
-          <StyledTableRow>
-            <StyledTableCell>Id</StyledTableCell>
-            <StyledTableCell>Name</StyledTableCell>
-            <StyledTableCell>Code</StyledTableCell>
-            <StyledTableCell>Type</StyledTableCell>
-          </StyledTableRow>
-        </TableHead>
-        <TableBody>
-          {accounts.map((account: Account) => (
-            <StyledTableRow key={account.id}>
-              <StyledTableCell>{account.id}</StyledTableCell>
-              <StyledTableCell>{account.name}</StyledTableCell>
-              <StyledTableCell>{account.code}</StyledTableCell>
-              <StyledTableCell>{account.type}</StyledTableCell>
+    <>
+      {snackbarType && (
+        <Snackbar
+          vertical='top'
+          horizontal='center'
+          isOpen={!!snackbarType}
+          onClose={() => setSnackbarType(null)}
+          autoHideDuration={4000}
+          key={'top' + 'center'}
+          message={snackbarType === 'success' ? 'Successfully deleted' : 'Failed to delete'}
+          severity={snackbarType}
+          variant='filled'
+        />
+      )}
+      <StyledWrapperFlex $direction='column' $gap='15px'>
+        <div>
+          <StyledTitleSpan>Accounts setting</StyledTitleSpan>
+        </div>
+        <Table>
+          <TableHead>
+            <StyledTableRow>
+              <StyledTableCell>Id</StyledTableCell>
+              <StyledTableCell>Name</StyledTableCell>
+              <StyledTableCell>Code</StyledTableCell>
+              <StyledTableCell>Type</StyledTableCell>
+              <StyledTableCell>Delete</StyledTableCell>
             </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </StyledWrapperFlex>
+          </TableHead>
+          <TableBody>
+            {accounts.map((account: Account) => (
+              <StyledTableRow key={account.id}>
+                <StyledTableCell>{account.id}</StyledTableCell>
+                <StyledTableCell>{account.name}</StyledTableCell>
+                <StyledTableCell>{account.code}</StyledTableCell>
+                <StyledTableCell>{account.type}</StyledTableCell>
+                <StyledTableCell>
+                  {!account.isDefaultAccount && (
+                    <Button color='inherit' onClick={() => handleDeleteAccount(account.id)}>
+                      Delete
+                    </Button>
+                  )}
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </StyledWrapperFlex>
+    </>
   )
 }
 
