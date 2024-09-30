c1f577fc51e5c1f9c7353b0e1446ec75ddb0fff6
Takanobu Suzuki <nobublack555@gmail.com>
2024-09-29 18:48:08 -0700

    fix error

--- src/features/login/schema.ts
+++ src/features/login/schema.ts
@@ -2,7 +2,7 @@ import * as z from 'zod'
 
 export const loginSchema = z.object({
   email: z.string().min(1, { message: 'Email is required' }),
-  password: z.string().min(1, { message: 'Confirm Password is required' }),
+  password: z.string().min(1, { message: 'Password is required' }),
 })
 
 export type LoginSchemaType = z.infer<typeof loginSchema>
